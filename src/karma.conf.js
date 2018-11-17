307e5c80051659c189f2e0ccb61fe5c9f34dd8ab
agoia <amalia.goia@catalysts.cc>
2018-11-17 17:28:11 +0100

    new layout

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -10,10 +10,23 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-scss-preprocessor')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false// leave Jasmine Spec Runner output visible in browser
+    },
+    files: [
+      {
+        pattern: '../node_modules/@angular/material/prebuilt-themes/deeppurple-amber.css',
+        included: true,
+        watched: true
+      },
+      {pattern: './test.ts', watched: false},
+      {pattern: './app/styles/**/*.*', watched: true, included: true, served: true}
+    ],
+    preprocessors: {
+      './app/styles/**/*.*': ['scss']
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
